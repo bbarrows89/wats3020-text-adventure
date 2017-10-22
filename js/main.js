1e0aa223b9e08bac442fee6a2aad5acc7881d9ba
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2017-10-22 12:48:58 -0700

    created prompt for character name, and function to get current page.

--- js/main.js
+++ js/main.js
@@ -11,13 +11,17 @@ let currentPage = null;
 
 // TODO: Prompt the user for their name. Store the name in the variable `playerName`.
 
+playerName = prompt('What is your character\'s name?');
 
 
 // TODO: Create a function called `getCurrentPage()`. It should accept one
 // parameter, which is the `slug` for the current page. This function will fetch
 // the current page and return a page object using the `slug` value for a key.
 
-
+function getCurrentPage(slug){
+  currentPage = storyData[slug];
+  return currentPage;
+}
 
 // TODO: Create a function called `recordChoice()` that will accept a `slug`
 // parameter and add it to the `choiceList` Array (probably using `push()`).
